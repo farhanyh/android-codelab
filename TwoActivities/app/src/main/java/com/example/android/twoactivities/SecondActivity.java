66213cad6723883137a5aa5569ef3d3d7bd2087c
Farhan Yahya Hidayat <farhan.yh@gmail.com>
2019-02-22 16:28:03 +0700

    20190222

--- TwoActivities/app/src/main/java/com/example/android/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.android.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     private EditText mReply;
 
@@ -21,6 +24,44 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     public void returnReply(View view) {
@@ -29,6 +70,7 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent(this, MainActivity.class);
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
